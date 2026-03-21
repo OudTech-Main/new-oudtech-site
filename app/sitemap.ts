9a5ec05cad3a00ca04264e1f1c22e497c66b43a2
Gospel Amanze <ollygos@gmail.com>
2026-03-21 13:24:34 +0100

    Feat: changes to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,7 @@
 import type { MetadataRoute } from "next";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? "https://oudtech.com";
+const BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL ?? "https://oudtechnologies.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
